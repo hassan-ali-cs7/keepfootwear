b9568bfae45afdd71678994be5a072e34af3d5df
hassanali11100 <hassanali11100@gmail.com>
2019-12-14 09:52:25 +0500

    images updated

--- index.php
+++ index.php
@@ -71,7 +71,8 @@ an error sending your message. Please try again later.</div>';
     <title>Coffee Shop Growth: Websites, SEO and More</title>
 
        <!-- CSS Stylesheets -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <link rel="stylesheet" type="text/css" href="css/navbar.css" />
     <link rel="stylesheet" type="text/css" href="css/get-a-quote-form.css" />
@@ -408,7 +409,10 @@ an error sending your message. Please try again later.</div>';
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
 
-    <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+
+    <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
   </body>
